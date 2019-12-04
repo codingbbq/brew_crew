1b42556174ea4964c4cf0646c820ba1338d66ce8
Codding BBQ <codingbbq@gmail.com>
2019-12-04 20:18:40 +0530

    Adding firestore and brew models to display brew listing

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,28 +1,35 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
-
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text("Brew Crew"),
-          elevation: 0.0,
-          backgroundColor: Colors.brown[400],
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              onPressed: () async {
-                await _auth.SignOut();
-              },
-              label: Text('logout'),
-            )
-          ],
+      child: StreamProvider<List<Brew>>.value(
+        value: DatabaseService().brews,
+        child: Scaffold(
+          backgroundColor: Colors.brown[50],
+          appBar: AppBar(
+            title: Text("Brew Crew"),
+            elevation: 0.0,
+            backgroundColor: Colors.brown[400],
+            actions: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                onPressed: () async {
+                  await _auth.SignOut();
+                },
+                label: Text('logout'),
+              )
+            ],
+          ),
+          body: BrewList(),
         ),
       )
     );
